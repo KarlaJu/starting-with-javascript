f81668112eab386f2c7f0e530b09759acc138357
Karla <karla@makingdevs.com>
2019-04-17 12:08:33 -0500

    Añadiendo bootstrap link, cambiando estilos con la función getElementId y cambiando el script a un archivo a parte

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-alert('It is working');
+var h1 = document.getElementById("title");
+var paragraph = document.getElementById("paragraph");
+var button = document.getElementById("change_button");
+
+console.log(h1);
+console.log(h1.innerHTML);
+button.onclick = function(){
+  h1.innerHTML = "A new title by innerHTML";
+  h1.style.color = "purple";
+  h1.style.background = "green";
+  paragraph.style.color = "olive";
+}
