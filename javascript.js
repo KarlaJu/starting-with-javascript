b7df0af9d94ce34803a82fafb1404d1b1be31e04
Karla <karla@makingdevs.com>
2019-04-17 16:47:58 -0500

    Agregando objetos y arreglo de objetos

--- javascript.js
+++ javascript.js
@@ -5,10 +5,37 @@ var paragraph_class = document.getElementsByClassName("lead");
 var all_p_tags = document.querySelectorAll('p');
 var greeting = document.getElementById('greeting');
 var sum_result = document.getElementById("sum_result");
+var object = {
+  name: 'HTML',
+  long: 15,
+  state: true,
+  chapters: {
+    name: 'Introduction',
+    videos: 20 
+  }
+}
+var object_array = [
+  {
+    name: 'HTML',
+    state: true
+  },
+  {
+    name: 'CSS',
+    state: true
+  },
+  {
+    name: 'JS',
+    state: false
+  }
+]
 
 console.log(h1);
 console.log(h1.innerHTML);
 console.log(all_p_tags);
+console.log('This is the our object: ',object);
+console.log('Chapters from the object: ', object.chapters);
+console.log('This is an object array: ', object_array);
+
 function change_style (){
   h1.innerHTML = "A new title by innerHTML";
   h1.style.color = "purple";
