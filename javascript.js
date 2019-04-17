25ec629490a1e073c18b1b3626f8f1023ef4258e
Karla <karla@makingdevs.com>
2019-04-17 13:10:17 -0500

    Añadiendo las funciones getElementByClassName y querySelectorAll

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 var h1 = document.getElementById("title");
 var paragraph = document.getElementById("paragraph");
 var button = document.getElementById("change_button");
+var paragraph_class = document.getElementsByClassName("lead");
+var all_p_tags = document.querySelectorAll('p');
 
 console.log(h1);
 console.log(h1.innerHTML);
+console.log(all_p_tags);
 function change_style (){
   h1.innerHTML = "A new title by innerHTML";
   h1.style.color = "purple";
   h1.style.background = "green";
   paragraph.style.color = "olive";
-}
+};
 
 button.onclick = change_style;
