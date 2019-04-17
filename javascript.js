f22996ef9ec65b19f5c874fc4889317556e29381
Karla <karla@makingdevs.com>
2019-04-17 12:12:53 -0500

    Cambiando la función anónima por la función definida change_style

--- javascript.js
+++ javascript.js
@@ -4,9 +4,11 @@ var button = document.getElementById("change_button");
 
 console.log(h1);
 console.log(h1.innerHTML);
-button.onclick = function(){
+function change_style (){
   h1.innerHTML = "A new title by innerHTML";
   h1.style.color = "purple";
   h1.style.background = "green";
   paragraph.style.color = "olive";
 }
+
+button.onclick = change_style;
