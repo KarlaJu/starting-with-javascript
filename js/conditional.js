2d7fdcdbac0c23110da7b2ee88ac2193bc62db3a
Karla <karla@makingdevs.com>
2019-04-26 16:37:43 -0500

    Una condicional simple

---
+++ js/conditional.js
@@ -0,0 +1,9 @@
+let user_welcome = "Ruby";
+
+console.log('--------------Conditional-----------');
+
+if(user_welcome == "Ruby"){
+  console.log(`Welcome ${user_welcome}`);
+} else{
+  console.log(`Welcome User`);
+}
