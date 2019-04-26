b8e5cc38797ddd3ea600e544d7bbe3b55d246eb8
Karla <karla@makingdevs.com>
2019-04-26 13:12:55 -0500

    Agregando elementos a un arreglo constante

--- js/arrays.js
+++ js/arrays.js
@@ -1,8 +1,10 @@
 
 const array_example = ['Alice', 23, 'Mexico', 'Developer'];
 const [name, age, country, profession = 'It was not specified'] = array_example;
+array_example.push('CDMX');
 
 console.log('----------------Array Examples----------------------------');
 console.log(`You can see array elements in this way: ${array_example[0]}`);
 console.log(`Also you can see the name using this way: ${name}`);
-console.log(`The profession too: ${profession}`)
+console.log(`The profession too: ${profession}`);
+console.log(`You can add elements to the array: ${array_example}`);
