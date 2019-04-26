d3e7b347dccb2f2699685fa679d8f7282844bbd0
Karla <karla@makingdevs.com>
2019-04-26 16:49:59 -0500

    Ejemplo de condicional usando tres signos de igual, comprueba el tipo de dato

--- js/conditional.js
+++ js/conditional.js
@@ -1,4 +1,5 @@
 let user_welcome = "Kaiser";
+let numeric_variable = 20;
 
 console.log('--------------Conditional-----------');
 
@@ -6,4 +7,10 @@ if(user_welcome == "Ruby"){
   console.log(`Welcome ${user_welcome}`);
 } else if(user_welcome == "Kaiser"){
   console.log(`Welcome Kaiser`);
+}
+
+if(numeric_variable === "20"){
+  console.log("The numeric variable is a string type");
+} else{
+  console.log("The numeric variable is a numeric type");
 }
