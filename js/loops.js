01f1decca2dce861f0b00b9f356bb16db3fa3714
KarlaJu <karla@makingdevs.com>
2019-04-27 19:20:54 -0500

    Ejemplo de ciclos usando do while

--- js/loops.js
+++ js/loops.js
@@ -10,3 +10,9 @@ while(iterator <= 5){
   console.log(`The result using WHILE is: ${iterator}`);
   iterator++;
 }
+
+iterator = 1;
+do{
+  console.log(`The result using DO WHILE is: ${iterator}`);
+  iterator++;
+}while(iterator <= 5);
