aeb45926d81bd372658cfb64f11451191a699f1d
Karla <karla@makingdevs.com>
2019-04-30 10:48:38 -0500

    Usando indexOf para saber el lugar exacto de un carácter dentro de la cadena

--- js/strings.js
+++ js/strings.js
@@ -5,3 +5,4 @@ console.log(`The string: ${string_song}`);
 console.log(`You can see how many characters has th string: ${string_song.length}`);
 console.log(`You can cut the string form the beginning: ${string_song.substring(15)}`);
 console.log(`You can cut where you want: ${string_song.substr(0, 15)}`);
+console.log(`You can get the place of a character, in this exaple 'e': ${string_song.indexOf("e")}`);
