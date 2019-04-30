355df63537af147d95096ce415180963cde7eab5
Karla <karla@makingdevs.com>
2019-04-30 11:08:45 -0500

    Usando indexOf para buscar la posición de un carácter desde cierto punto en la cadena

--- js/strings.js
+++ js/strings.js
@@ -6,3 +6,4 @@ console.log(`You can see how many characters has th string: ${string_song.length
 console.log(`You can cut the string form the beginning: ${string_song.substring(15)}`);
 console.log(`You can cut where you want: ${string_song.substr(0, 15)}`);
 console.log(`You can get the place of a character, in this exaple 'e': ${string_song.indexOf("e")}`);
+console.log(`If you have more than one character, like 'o', you can search for it from the place you want: ${string_song.indexOf("o", 2)}`);
