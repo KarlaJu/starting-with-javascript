47ca5aa006e1ab39c2b76a5c9fc874549c565275
Karla <karla@makingdevs.com>
2019-04-30 11:18:59 -0500

    Reemplazando toda la cadena por una nueva utilizando el método replace

--- js/strings.js
+++ js/strings.js
@@ -8,3 +8,4 @@ console.log(`You can cut where you want: ${string_song.substr(0, 15)}`);
 console.log(`You can get the place of a character, in this exaple 'e': ${string_song.indexOf("e")}`);
 console.log(`If you have more than one character, like 'o', you can search for it from the place you want: ${string_song.indexOf("o", 2)}`);
 console.log(`Searching 'o' from the end: ${string_song.lastIndexOf("o")}`);
+console.log(`You can replace the text: ${string_song.replace(string_song, "I fallen in love")}`);
