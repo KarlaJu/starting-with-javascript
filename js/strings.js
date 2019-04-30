1c157f2afcc31fe1ffba17dfc90214486569e6d0
Karla <karla@makingdevs.com>
2019-04-30 10:34:13 -0500

    Usando método substring paraa cortar la cadena empezando desde el principio

--- js/strings.js
+++ js/strings.js
@@ -3,5 +3,5 @@ let string_song = "Love of my life you hurt me, you broken my heart now you leav
 console.log('-----------String properties----------');
 console.log(`The string: ${string_song}`);
 console.log(`You can see how many characters has th string: ${string_song.length}`);
-
+console.log(`You can cut the string form the beginning: ${string_song.substring(15)}`);
 
